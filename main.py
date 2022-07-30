6c627d61a665515273ec64f59a14967816734521
mahesh <maheshraju.9381@gmail.com>
2022-07-30 22:34:53 +0530

    initial snake

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from turtle import Turtle,Screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
+screen.title('snakeGame')
+positions = [0, 20, 40]
+for i in range(3):
+    new_square = Turtle()
+    new_square.color("white")
+    new_square.shape('square')
+    new_square.penup()
+    new_square.goto(positions[i-1], 0)
+
+
+
+
 
 
 
 screen.exitonclick()
+
