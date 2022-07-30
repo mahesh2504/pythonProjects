13088d507d76ad69bb1874babd43cf8988d330ab
mahesh <maheshraju.9381@gmail.com>
2022-07-30 22:40:55 +0530

    final initial snake

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('snakeGame')
-positions = [0, 20, 40]
-for i in range(3):
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+for position in starting_positions:
     new_square = Turtle()
     new_square.color("white")
     new_square.shape('square')
     new_square.penup()
-    new_square.goto(positions[i-1], 0)
+    new_square.goto(position)
+
 
 
 
